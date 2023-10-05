d25ed8f5b8c8feabca8500ce8fa01523aae4f4aa
farhern <farhanjp7452@gmail.com>
2023-10-05 17:13:59 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={87971abb6744607c4a1b507ee488acb6}`
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
