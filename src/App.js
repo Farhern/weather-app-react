0b4fac102d99abe9f70b31ccd063c0828fe9807c
farhern <farhanjp7452@gmail.com>
2023-10-09 11:03:33 +0300

    made api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=87971abb6744607c4a1b507ee488acb6`
 
-  //const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=87971abb6744607c4a1b507ee488acb6`
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
   return (
     <div className='app'>
+      <div className="search">
+        <input
+          value={location} 
+          onKeyPress={searchLocation}
+          onChange={event => setLocation(event.target.value)}
+          placeholder='Enter Location'
+          type="text" 
+        />
+      </div>
       <div className="container">
       <div className="top">
         <div className="location">
