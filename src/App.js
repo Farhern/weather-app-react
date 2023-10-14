65d76aedd768e2ee9e1869298aeff0806b0db8d1
farhern <farhanjp7452@gmail.com>
2023-10-14 12:16:57 +0300

    added api

--- src/App.js
+++ src/App.js
@@ -30,26 +30,26 @@ function App() {
       <div className="container">
       <div className="top">
         <div className="location">
-          <p>Nairobi</p>
+          <p>{data.name}</p>
         </div>
         <div className="temp">
-          <h1>65°F</h1>
+        {data.main ? <h1>{data.main.temp}°F</h1> :null}
         </div>
         <div className="description">
-          <p>Clouds</p>
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
         </div>
       </div>
       <div className="bottom">
         <div className="feels">
-        <p className='bold'>65°F</p>
-        <p>Feels Like</p>
+       {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+       <p>Feels Like</p>
         </div>
         <div className="humidity">
-          <p className='bold'>20%</p>
+        {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
           <p>Humidity</p>
         </div>
         <div className="wind">
-          <p className='bold'>12 MPH</p>
+        {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
           <p>Wind Speed</p>
         </div>
       </div>
