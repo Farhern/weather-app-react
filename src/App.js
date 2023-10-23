d8a23b28ea1a4690ee8477fbb5c3c1ab8278f8ae
farhern <farhanjp7452@gmail.com>
2023-10-23 16:42:25 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=87971abb6744607c4a1b507ee488acb6`
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid=87971abb6744607c4a1b507ee488acb6`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
@@ -33,26 +33,32 @@ function App() {
           <p>{data.name}</p>
         </div>
         <div className="temp">
-        {data.main ? <h1>{data.main.temp}°F</h1> :null}
+        {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> :null}
         </div>
         <div className="description">
           {data.weather ? <p>{data.weather[0].main}</p> : null}
         </div>
       </div>
-      <div className="bottom">
-        <div className="feels">
-       {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
-       <p>Feels Like</p>
-        </div>
-        <div className="humidity">
-        {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-          <p>Humidity</p>
-        </div>
-        <div className="wind">
-        {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-          <p>Wind Speed</p>
-        </div>
-      </div>
+
+    {data.name !== undefined && 
+       <div className="bottom">
+       <div className="feels">
+      {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+      <p>Feels Like</p>
+       </div>
+       <div className="humidity">
+       {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+         <p>Humidity</p>
+       </div>
+       <div className="wind">
+       {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+         <p>Wind Speed</p>
+       </div>
+     </div>
+    }
+      
+
+
       </div>
     </div>
   );
